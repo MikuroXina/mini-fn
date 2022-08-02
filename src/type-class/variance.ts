d5991bfc1d805fa4eae276ad231e2397ad9d01d6
MikuroXina <ryosukadnak@gmail.com>
2022-08-02 16:54:29 +0900

    fix: Fix Contravariant

--- src/type-class/variance.ts
+++ src/type-class/variance.ts
@@ -1,5 +1,5 @@
-import type { HktKeyA1, HktDictA1 } from "../hkt";
+import type { HktKeyA1, GetHktA1 } from "../hkt";
 
 export interface Contravariant<S extends HktKeyA1> {
-    contraMap<T1, U1>(f: (arg: Readonly<T1>) => U1): (u: HktDictA1<U1>) => HktDictA1<T1>;
+    contraMap<T1, U1>(f: (arg: Readonly<T1>) => U1): (u: GetHktA1<S, U1>) => GetHktA1<S, T1>;
 }
