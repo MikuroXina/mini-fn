a233162e6e17264489689e8ca167889ab7adac75
MikuroXina <ryosukadnak@gmail.com>
2022-08-14 12:59:14 +0900

    fix: Improve Ordering

--- src/ordering.ts
+++ src/ordering.ts
@@ -1,24 +1,34 @@
 import type { Monoid } from "./type-class/monoid";
 
 export const less = -1;
+export type Less = typeof less;
 export const equal = 0;
+export type Equal = typeof equal;
 export const greater = 1;
-export type Ordering = typeof less | typeof equal | typeof greater;
+export type Greater = typeof greater;
+export type Ordering = Less | Equal | Greater;
 
-export const isLt = (ord: Ordering) => ord === less;
-export const isGt = (ord: Ordering) => ord === greater;
-export const isLe = (ord: Ordering) => ord !== greater;
-export const isGe = (ord: Ordering) => ord !== less;
-export const isEq = (ord: Ordering) => ord == equal;
-export const isNe = (ord: Ordering) => ord != equal;
+export const isLt = (ord: Ordering): ord is Less => ord === less;
+export const isGt = (ord: Ordering): ord is Greater => ord === greater;
+export const isLe = (ord: Ordering): ord is Less | Equal => ord !== greater;
+export const isGe = (ord: Ordering): ord is Greater | Equal => ord !== less;
+export const isEq = (ord: Ordering): ord is Equal => ord == equal;
+export const isNe = (ord: Ordering): ord is Less | Greater => ord != equal;
 
-export const reverse = (order: Ordering): Ordering => -order as Ordering;
+export type Reversed<O extends Ordering> = O extends Less
+    ? Greater
+    : O extends Greater
+    ? Less
+    : O extends Equal
+    ? Equal
+    : Ordering;
+export const reverse = <O extends Ordering>(order: O): Reversed<O> => -order as Reversed<O>;
 
-export const then = (first: Ordering, second: Ordering) => (first === equal ? second : first);
-export const thenWith = (first: Ordering, secondFn: () => Ordering) =>
+export const then = (second: Ordering) => (first: Ordering) => first === equal ? second : first;
+export const thenWith = (secondFn: () => Ordering) => (first: Ordering) =>
     first === equal ? secondFn() : first;
 
 export const monoid: Monoid<Ordering> = {
-    combine: then,
+    combine: (l, r) => then(r)(l),
     identity: equal,
 };
