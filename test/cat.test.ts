74e78ebe4f4aa222becd5ea8cbf6c22b9956ccaf
MikuroXina <ryosukadnak@gmail.com>
2022-08-14 16:39:00 +0900

    ci: Setup first test

---
+++ test/cat.test.ts
@@ -0,0 +1,8 @@
+import { Cat } from "../src/lib";
+
+test("Cat", () => {
+    const result = Cat.cat(-3)
+        .feed((x) => x ** 2)
+        .feed((x) => x.toString());
+    expect(result.value).toBe("9");
+});
