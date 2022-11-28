3a1b91204a813fb8b0d45292cca88d73c6ef900b
MikuroXina <ryosukadnak@gmail.com>
2022-11-28 11:39:44 +0900

    Add rest of MonadTrans

--- src/trans.ts
+++ src/trans.ts
@@ -1,5 +1,5 @@
-import type { GetHktA1, GetHktA2 } from "hkt";
-import type { Monad1, Monad2, Monad2Monoid } from "type-class/monad";
+import type { GetHktA1, GetHktA2, GetHktA3, GetHktA4 } from "hkt";
+import type { Monad1, Monad2, Monad2Monoid, Monad3, Monad4 } from "type-class/monad";
 
 export interface MonadTrans1<T> {
     lift: <M>(monad: Monad1<M>) => <A>(ma: GetHktA1<M, A>) => GetHktA1<T, GetHktA1<M, A>>;
@@ -9,4 +9,10 @@ export interface MonadTrans2<T> {
 }
 export interface MonadTrans2Monoid<T, M> {
     lift: (monad: Monad2Monoid<T, M>) => <A>(ma: GetHktA1<M, A>) => GetHktA2<T, M, A>;
+}
+export interface MonadTrans3<T> {
+    lift: <M>(monad: Monad3<M>) => <B, A>(ma: GetHktA2<M, B, A>) => GetHktA3<T, M, B, A>;
+}
+export interface MonadTrans4<T> {
+    lift: <M>(monad: Monad4<M>) => <C, B, A>(ma: GetHktA3<M, C, B, A>) => GetHktA4<T, M, C, B, A>;
 }
