c100a7e729e808e6960b588599c68c8218aaa5f6
Mikuroさいな <ryosukadnak@gmail.com>
2022-11-28 21:46:20 +0900

    test: Add test of twenty times by State monad (#12)

--- test/state.test.ts
+++ test/state.test.ts
@@ -25,3 +25,20 @@ test("roll three dices", () => {
         result3: -1370047078,
     });
 });
+
+test("twenty times", () => {
+    const twentyTimes = (x: number): number =>
+        Cat.cat(State.put(x + x))
+            .feed(State.flatMap(State.get<number>))
+            .feed(State.map((x2: number) => x2 + x2))
+            .feed(State.map((x4: number) => x4 + x4))
+            .feed(
+                State.flatMap((x8: number) =>
+                    State.map<number, number, number>((x2: number) => x8 + x2)(State.get<number>()),
+                ),
+            )
+            .feed(State.map((x10: number) => x10 + x10))
+            .value(0)[0];
+
+    expect(twentyTimes(10)).toBe(200);
+});
