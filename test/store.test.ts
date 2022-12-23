b852e5bcd9ef84b31490d9415b0cfb82ebc4fc82
MikuroXina <ryosukadnak@gmail.com>
2022-12-23 23:09:24 +0900

    Fix test case

--- test/store.test.ts
+++ test/store.test.ts
@@ -24,7 +24,7 @@ test("store with life game", () => {
     const neighborsOf = ([x, y]: Coord): List<Coord> =>
         fromArray(
             [-1, 0, 1]
-                .flatMap((dx) => [-1, 0, 1].flatMap<Coord>((dy) => [x + dx, y + dy]))
+                .flatMap((dx) => [-1, 0, 1].map<Coord>((dy) => [x + dx, y + dy]))
                 .filter(([px, py]) => !(px == 0 && py == 0)),
         );
     const neighborCells: (plane: CellPlane<Conway>) => List<Conway> =
@@ -42,19 +42,26 @@ test("store with life game", () => {
     };
     const evolve = extend(comonad)(step);
 
-    const toCellPlane = (record: Map<Coord, Conway>): CellPlane<Conway> => ({
+    const toCellPlane = (record: Conway[][]): CellPlane<Conway> => ({
         index: [0, 0],
-        accessor: (index) => record.get(index) ?? "Dead",
+        accessor: ([x, y]) => record?.[y]?.[x] ?? "Dead",
     });
     const fromCellPlane =
         (plane: CellPlane<Conway>) =>
-        ({ width, height }: Area): Map<Coord, Conway> => {
+        ({ width, height }: Area): Conway[][] => {
             const xs = toArray(range(0, width));
             const ys = toArray(range(0, height));
             const coords = fromArray(ys.flatMap((y) => xs.map((x): Coord => [x, y])));
-            return new Map(
-                toIterator(zip(coords)(experiment(listFunctor)<Coord>(constant(coords))(plane))),
-            );
+            const array: Conway[][] = [];
+            for (const [[x, y], cell] of toIterator(
+                zip(coords)(experiment(listFunctor)<Coord>(constant(coords))(plane)),
+            )) {
+                if (!array[y]) {
+                    array[y] = [];
+                }
+                array[y][x] = cell;
+            }
+            return array;
         };
 
     /*
@@ -63,12 +70,12 @@ test("store with life game", () => {
     |OOO |
     |    |
     */
-    const mapping = new Map<Coord, Conway>();
-    mapping.set([1, 0], "Alive");
-    mapping.set([2, 1], "Alive");
-    mapping.set([0, 2], "Alive");
-    mapping.set([1, 2], "Alive");
-    mapping.set([2, 2], "Alive");
+    const mapping: Conway[][] = [
+        ["Dead", "Alive", "Dead", "Dead"],
+        ["Dead", "Dead", "Alive", "Dead"],
+        ["Alive", "Alive", "Alive", "Dead"],
+        ["Dead", "Dead", "Dead", "Dead"],
+    ];
     const initialPlane = toCellPlane(mapping);
     const evolved = evolve(initialPlane);
     /*
@@ -78,9 +85,10 @@ test("store with life game", () => {
     | O  |
     */
     const nextPlane = fromCellPlane(evolved)({ width: 4, height: 4 });
-    expect(nextPlane.get([0, 1])).toEqual(true);
-    expect(nextPlane.get([2, 1])).toEqual(true);
-    expect(nextPlane.get([1, 2])).toEqual(true);
-    expect(nextPlane.get([2, 2])).toEqual(true);
-    expect(nextPlane.get([1, 3])).toEqual(true);
+    expect(nextPlane).toEqual([
+        ["Dead", "Dead", "Dead", "Dead"],
+        ["Alive", "Dead", "Alive", "Dead"],
+        ["Dead", "Alive", "Alive", "Dead"],
+        ["Dead", "Alive", "Dead", "Dead"],
+    ]);
 });
