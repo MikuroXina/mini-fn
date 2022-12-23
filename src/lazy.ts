fa58cc48e7e4a403471a87a8e7699f20bca72a03
MikuroXina <ryosukadnak@gmail.com>
2022-12-23 13:20:27 +0900

    Impl Functor for Lazy

--- src/lazy.ts
+++ src/lazy.ts
@@ -1,7 +1,9 @@
 import { Eq, PartialEq, eqSymbol } from "./type-class/eq.js";
 
-const lazyNominal = Symbol("Lazy");
+import type { Functor1 } from "./type-class/functor.js";
 
+const lazyNominal = Symbol("Lazy");
+export type LazyHktKey = typeof lazyNominal;
 export interface Lazy<L> {
     [lazyNominal]: () => L;
 }
@@ -17,3 +19,18 @@ export const eq = <T>(equality: Eq<T, T>): Eq<Lazy<T>, Lazy<T>> => ({
     ...partialEq(equality),
     [eqSymbol]: true,
 });
+
+export const map =
+    <A, B>(fn: (a: A) => B) =>
+    (lazy: Lazy<A>): Lazy<B> =>
+        defer(() => fn(force(lazy)));
+
+declare module "./hkt.js" {
+    export interface HktDictA1<A1> {
+        [lazyNominal]: Lazy<A1>;
+    }
+}
+
+export const functor: Functor1<LazyHktKey> = {
+    map,
+};
