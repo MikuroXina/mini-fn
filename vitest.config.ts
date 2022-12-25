175a8a75737dd1e5455196d830d34f124a1cab78
MikuroXina <ryosukadnak@gmail.com>
2022-12-25 21:47:49 +0900

    Mix test files into src

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
     test: {
         coverage: {
-            exclude: [".pnp.*", "test/**/*.ts"],
+            exclude: ["src/**/*.test.ts"],
             reporter: ["text", "clover"],
             reportsDirectory: "coverage",
         },
