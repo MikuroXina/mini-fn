4ae6c6fab44c965b2c49a870b8afc5be375f8c98
MikuroXina <ryosukadnak@gmail.com>
2022-12-25 23:12:50 +0900

    Enforce test

--- src/cat.test.ts
+++ src/cat.test.ts
@@ -1,10 +1,41 @@
+import { Cat, apply, cat, flatMap, inspect, map, product } from "./cat.js";
 import { expect, test } from "vitest";
 
-import { cat } from "./cat.js";
-
-test("Cat", () => {
+test("cat", () => {
     const result = cat(-3)
         .feed((x) => x ** 2)
         .feed((x) => x.toString());
     expect(result.value).toBe("9");
+});
+
+test("inspect", () => {
+    const result = cat(-3)
+        .feed(inspect((x) => expect(x).toBe(-3)))
+        .feed((x) => x ** 2)
+        .feed(inspect((x) => expect(x).toBe(9)))
+        .feed((x) => x.toString())
+        .feed(inspect((x) => expect(x).toBe("9")));
+    expect(result.value).toBe("9");
+});
+
+test("product", () => {
+    const actual = product(cat(5))(cat("foo")).value;
+    expect(actual).toEqual([5, "foo"]);
+});
+
+test("map", () => {
+    const actual = map((v: number) => v / 2)(cat(10)).value;
+    expect(actual).toBe(5);
+});
+
+test("flatMap", () => {
+    const sub = (num: number): Cat<string> => cat(num).feed((x) => x.toString());
+    const actual = flatMap(sub)(cat(6)).value;
+    expect(actual).toBe("6");
+});
+
+test("apply", () => {
+    const sub = cat((numeral: string) => parseInt(numeral, 10));
+    const actual = apply(sub)(cat("1024")).value;
+    expect(actual).toBe(1024);
 });
