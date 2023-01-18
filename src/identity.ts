1f4949667bd39c9663887e51ad1f69dfa22cf5f0
MikuroXina <ryosukadnak@gmail.com>
2023-01-19 00:35:38 +0900

    Add distributive and settable

--- src/identity.ts
+++ src/identity.ts
@@ -1,7 +1,9 @@
 import { flip, id } from "./func.js";
 
+import type { Distributive } from "./type-class/distributive.js";
 import type { Hkt1 } from "./hkt.js";
 import type { Monad } from "./type-class/monad.js";
+import type { Settable } from "./type-class/settable.js";
 import type { Traversable } from "./type-class/traversable.js";
 import { make } from "./tuple.js";
 
@@ -27,4 +29,16 @@ export const traversable: Traversable<IdentityHkt> = {
     map: id,
     foldR: flip,
     traverse: () => id,
+};
+
+export const distributive: Distributive<IdentityHkt> = {
+    map: id,
+    distribute: () => id,
+};
+
+export const settable: Settable<IdentityHkt> = {
+    ...traversable,
+    ...monad,
+    ...distributive,
+    untainted: id,
 };
