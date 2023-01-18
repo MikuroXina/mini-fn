a4b71ec564ff34b401467f3d13e09fc1897d87e9
MikuroXina <ryosukadnak@gmail.com>
2023-01-18 15:20:52 +0900

    Add Choice

---
+++ src/type-class/choice.ts
@@ -0,0 +1,9 @@
+import type { Get2, Hkt2 } from "src/hkt.js";
+
+import type { Profunctor } from "./profunctor.js";
+import type { Result } from "src/result.js";
+
+export interface Choice<P extends Hkt2> extends Profunctor<P> {
+    readonly left: <A, B, C>(curr: Get2<P, A, B>) => Get2<P, Result<A, C>, Result<B, C>>;
+    readonly right: <A, B, C>(curr: Get2<P, A, B>) => Get2<P, Result<C, A>, Result<C, B>>;
+}
