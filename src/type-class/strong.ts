d079bb251637f76cd3f72e3503cebc0d20bf26b2
MikuroXina <ryosukadnak@gmail.com>
2023-01-21 15:27:27 +0900

    Fix Strong impl

--- src/type-class/strong.ts
+++ src/type-class/strong.ts
@@ -1,7 +1,7 @@
 import type { Get2, Hkt2 } from "../hkt.js";
+import { Profunctor, rightMap } from "./profunctor.js";
 
 import type { Category } from "./category.js";
-import type { Profunctor } from "./profunctor.js";
 
 export interface Strong<S extends Hkt2> extends Profunctor<S> {
     readonly first: <A, B, C>(m: Get2<S, A, B>) => Get2<S, [A, C], [B, C]>;
@@ -12,13 +12,15 @@ export const split =
     <S extends Hkt2>(str: Strong<S>, cat: Category<S>) =>
     <A, B>(funcA: Get2<S, A, B>) =>
     <C, D>(funcC: Get2<S, C, D>): Get2<S, [A, C], [B, D]> =>
-        cat.compose<[A, C], [B, C], [B, D]>(str.first<A, B, C>(funcA))(str.second<C, D, B>(funcC));
+        cat.compose(str.first<A, B, D>(funcA))<[A, C]>(str.second(funcC));
 
-export const fanOut = <S extends Hkt2>(str: Strong<S>, cat: Category<S>) => {
-    const splitSC = split(str, cat);
-    return <A, B>(funcB: Get2<S, A, B>) =>
-        <C>(funcC: Get2<S, A, C>): Get2<S, A, [B, C]> =>
-            cat.compose<A, [A, A], [B, C]>(
-                str.diMap((a: A) => a)((a: A): [A, A] => [a, a])(cat.identity()),
-            )(splitSC(funcB)(funcC));
-};
+export const fanOut =
+    <S extends Hkt2>(str: Strong<S>, cat: Category<S>) =>
+    <A, B>(funcB: Get2<S, A, B>): (<C>(funcC: Get2<S, A, C>) => Get2<S, A, [B, C]>) => {
+        const splitSC: <C, D>(func: Get2<S, C, D>) => Get2<S, [A, C], [B, D]> = split(
+            str,
+            cat,
+        )(funcB);
+        return <C>(funcC: Get2<S, A, C>): Get2<S, A, [B, C]> =>
+            cat.compose(splitSC(funcC))(rightMap(str)((a: A): [A, A] => [a, a])(cat.identity()));
+    };
