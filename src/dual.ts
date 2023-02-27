69fc34b249b6a9741e5ebec4c64052b49118e031
MikuroXina <ryosukadnak@gmail.com>
2023-02-28 01:12:58 +0900

    Add bifunctor for Dual

--- src/dual.ts
+++ src/dual.ts
@@ -1,4 +1,6 @@
-import type { Hkt2 } from "./hkt.js";
+import type { FnHkt } from "./func.js";
+import type { Get2, Hkt2 } from "./hkt.js";
+import type { GenericBifunctor } from "./type-class/bifunctor.js";
 import type { Category } from "./type-class/category.js";
 import type { Monoid } from "./type-class/monoid.js";
 import type { Contravariant } from "./type-class/variance.js";
@@ -27,4 +29,16 @@ export const contra: Contravariant<DualHkt> = {
 export const monoid = <A, B>(m: Monoid<A>): Monoid<Dual<A, B>> => ({
     identity: () => m.identity,
     combine: (f, g) => (b) => m.combine(f(b), g(b)),
+});
+
+export const bifunctor = <T>(
+    bf: GenericBifunctor<FnHkt, FnHkt, FnHkt, T>,
+): GenericBifunctor<DualHkt, DualHkt, DualHkt, T> => ({
+    cat1: cat,
+    cat2: cat,
+    cat3: cat,
+    genericBiMap:
+        <A, B>(f: Dual<A, B>) =>
+        <C, D>(g: Dual<C, D>): Dual<Get2<T, A, C>, Get2<T, B, D>> =>
+            bf.genericBiMap(f)(g),
 });
