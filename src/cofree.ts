b8b9670a2b98d036e2771c99e623f70cfc983b6b
MikuroXina <ryosukadnak@gmail.com>
2023-03-01 23:54:15 +0900

    Add docs for Cofree

--- src/cofree.ts
+++ src/cofree.ts
@@ -36,6 +36,9 @@ export interface CofreeHkt extends Hkt2 {
     readonly type: Cofree<this["arg2"], this["arg1"]>;
 }
 
+/**
+ * Co-free functor, the dual of `Free`. It is isomorphic to the linked list of `A`.
+ */
 export type Cofree<F, A> = Lazy<Tuple<A, Get1<F, Cofree<F, A>>>>;
 
 export const partialEquality = <F, A>({
@@ -97,30 +100,81 @@ export const cmp = <F, A>({
 };
 export const ord = fromCmp(cmp);
 
+/**
+ * Creates a new `Cofree` from the inner function.
+ *
+ * @param fn - The deferred function.
+ * @returns The new instance.
+ */
 export const defer: <F, A>(fn: () => [A, Get1<F, Cofree<F, A>>]) => Cofree<F, A> = lazyDefer;
 
+/**
+ * Creates a new `Cofree` from the head and successor.
+ *
+ * @param a - The head element.
+ * @param f - The successor, a `Cofree` mapped by `F`.
+ * @returns The new instance.
+ */
 export const make =
     <A>(a: A) =>
     <F>(f: Get1<F, Cofree<F, A>>): Cofree<F, A> =>
         lazyDefer(() => [a, f]);
 
+/**
+ * Gets the head element.
+ *
+ * @param c - The instance of `Cofree`.
+ * @returns The head element.
+ */
 export const head = <F, A>(c: Cofree<F, A>): A => force(c)[0];
 
+/**
+ * Gets the tail elements.
+ *
+ * @param c - The instance of `Cofree`.
+ * @returns The tail elements contained by `F`.
+ */
 export const tail = <F, A>(c: Cofree<F, A>): Get1<F, Cofree<F, A>> => force(c)[1];
 
+/**
+ * Unwraps the instance.
+ *
+ * @param c - The instance of `Cofree`.
+ * @returns The tail elements contained by the functor `F`.
+ */
 export const unwrap = <F extends Hkt1, A>(cofree: Cofree<F, A>): Get1<F, Cofree<F, A>> =>
     force(cofree)[1];
 
+/**
+ * Maps the function for `Cofree`.
+ *
+ * @param f - The instance of `Functor` for `F`.
+ * @param fn - The mapping function from `A` to `B`.
+ * @returns - The lifted function from `Cofree<F, A>` to `Cofree<F, B>`.
+ */
 export const map =
     <F extends Hkt1>(f: Functor<F>) =>
-    <A, B>(fn: (a: A) => B) =>
-    (c: Cofree<F, A>): Cofree<F, B> =>
+    <A, B>(fn: (a: A) => B): ((c: Cofree<F, A>) => Cofree<F, B>) =>
         lazyMap(([a, fa]: readonly [A, Get1<F, Cofree<F, A>>]): [B, Get1<F, Cofree<F, B>>] => [
             fn(a),
             f.map(map(f)(fn))(fa),
-        ])(c);
+        ]);
+/**
+ * Extracts from the instance.
+ *
+ * @param c - The instance of `Cofree`.
+ * @returns The head element.
+ */
 export const extract = head;
 
+/**
+ * Hoists the elements in `Cofree` by the natural transformation.
+ *
+ * @param f - The instance of `Functor` for `F`.
+ * @param nat - The natural transformation.
+ * @param c - The instance of `Cofree`.
+ * @returns The hoisted instance.
+ */
 export const hoist =
     <F extends Hkt1>(f: Functor<F>) =>
     <G>(nat: <T>(a: Get1<F, T>) => Get1<G, T>) =>
@@ -131,55 +185,118 @@ export const hoist =
             ),
         )(c);
 
+/**
+ * Build a new `Cofree` from `builder`.
+ *
+ * @param f - The instance of `Functor` for `F`.
+ * @param builder - The function constructs a tuple of head and successor.
+ * @param init - The initial value for `builder`.
+ * @returns The new instance.
+ */
 export const build =
     <F extends Hkt1>(f: Functor<F>) =>
     <S, A>(builder: (s: S) => [A, Get1<F, S>]) =>
     (init: S): Cofree<F, A> =>
         lazyDefer(() => tupleMap(f.map(build(f)(builder)))(builder(init)));
 
+/**
+ * Generates a new `Cofree` from the co-iteration and seed.
+ *
+ * @param functor - The instance of `Functor` for `F`.
+ * @param psi - The co-iteration, wraps the element by `F`.
+ * @param a - The seed for `psi`.
+ * @returns The new instance.
+ */
 export const coiter =
     <F extends Hkt1>(functor: Functor<F>) =>
     <A>(psi: (a: A) => Get1<F, A>) =>
     (a: A): Cofree<F, A> =>
         make(a)(functor.map(coiter(functor)(psi))(psi(a)));
 
+/**
+ * Generates a new `Cofree` from the co-iteration and seed wrapped with comonad `W`.
+ *
+ * @param comonad - The instance of `Comonad` for `W`.
+ * @param functor - The instance of `Functor` for `F`.
+ * @param psi - The co-iteration, wraps the element by `F`.
+ * @param a - The seed for `psi`.
+ * @returns The new instance.
+ */
 export const coiterW =
     <W extends Hkt1, F extends Hkt1>(comonad: Comonad<W>, functor: Functor<F>) =>
     <A>(psi: (wa: Get1<W, A>) => Get1<F, Get1<W, A>>) =>
     (a: Get1<W, A>): Cofree<F, A> =>
         make(comonad.extract(a))(functor.map(coiterW(comonad, functor)(psi))(psi(a)));
 
+/**
+ * Unfolds into a `Cofree` from the seed.
+ *
+ * @param f - The instance of `Functor` for `F`.
+ * @param unfolder - The function unfolds from `S` to `A`.
+ * @param init - The seed for `unfolder`.
+ * @returns The new instance.
+ */
 export const unfold =
     <F extends Hkt1>(f: Functor<F>) =>
-    <S, A>(fn: (s: S) => A) =>
-    (unfolder: (s: S) => Get1<F, S>): ((init: S) => Cofree<F, A>) =>
-        build(f)((s) => [fn(s), unfolder(s)]);
+    <S, A>(unfolder: (s: S) => Tuple<A, Get1<F, S>>) =>
+    (init: S): Cofree<F, A> => {
+        const [headS, tailS] = unfolder(init);
+        return make(headS)(f.map(unfold(f)(unfolder))(tailS));
+    };
 
+/**
+ * Unfolds into a `Cofree` from the seed with monad `M`.
+ *
+ * @param traversable - The instance of `Traversable` for `F`.
+ * @param monad - THe instance of `Monad` for `M`.
+ * @param unfolder - The function unfolds from `S` to `A` with monad `M`.
+ * @param init - The seed for `unfolder`.
+ * @returns The new instance.
+ */
 export const unfoldM = <F extends Hkt1, M extends Hkt1>(
     traversable: Traversable<F>,
     monad: Monad<M>,
 ) => {
     const partial = <B, A>(
-        f: (b: B) => Get1<M, Lazy<Tuple<A, Get1<F, B>>>>,
-    ): ((b: B) => Get1<M, Cofree<F, A>>) =>
-        kleisli(monad)(f)((tuple) => {
+        unfolder: (b: B) => Get1<M, Lazy<Tuple<A, Get1<F, B>>>>,
+    ): ((init: B) => Get1<M, Cofree<F, A>>) =>
+        kleisli(monad)(unfolder)((tuple) => {
             const [x, t] = force(tuple);
             return liftM(monad)(make(x))(
-                mapMTraversable<F, M, B, Cofree<F, A>>(traversable, monad)(partial(f))(t),
+                mapMTraversable<F, M, B, Cofree<F, A>>(traversable, monad)(partial(unfolder))(t),
             );
         });
     return partial;
 };
 
+/**
+ * Creates a new `Cofree` from the comonad `F`.
+ *
+ * @param comonad - The instance of `Comonad` for `F`.
+ * @param as - The seed used for construction.
+ * @returns The new instance.
+ */
 export const section =
     <F extends Hkt1>(comonad: Comonad<F>) =>
     <A>(as: Get1<F, A>): Cofree<F, A> =>
         make(comonad.extract(as))(extend(comonad)(section(comonad))(as));
 
+/**
+ * The instance of `Functor` for `Cofree<F, _>`, requires the `Functor` for `F`.
+ *
+ * @param f - The instance of `Functor` for `F`.
+ * @returns The instance of `Functor` for `Cofree<F, _>`.
+ */
 export const functor = <F extends Hkt1>(f: Functor<F>): Functor<Apply2Only<CofreeHkt, F>> => ({
     map: map(f),
 });
 
+/**
+ * The instance of `Comonad` for `Cofree<F, _>`, requires the `Functor` for `F`.
+ *
+ * @param f - The instance of `Functor` for `F`.
+ * @returns The instance of `Comonad` for `Cofree<F, _>`.
+ */
 export const comonad = <F extends Hkt1>(f: Functor<F>): Comonad<Apply2Only<CofreeHkt, F>> => {
     const duplicate = <A>(w: Cofree<F, A>): Cofree<F, Cofree<F, A>> =>
         make(w)(f.map(duplicate)(unwrap(w)));
@@ -190,6 +307,12 @@ export const comonad = <F extends Hkt1>(f: Functor<F>): Comonad<Apply2Only<Cofre
     };
 };
 
+/**
+ * The instance of `ComonadCofree` for `Cofree<F, _>`, requires the `Functor` for `F`.
+ *
+ * @param f - The instance of `Functor` for `F`.
+ * @returns The instance of `ComonadCofree` for `Cofree<F, _>`.
+ */
 export const comonadCofree = <F extends Hkt1>(
     f: Functor<F>,
 ): ComonadCofree<F, Apply2Only<CofreeHkt, F>> => ({
@@ -198,6 +321,12 @@ export const comonadCofree = <F extends Hkt1>(
     unwrap,
 });
 
+/**
+ * The instance of `Representable` for `Cofree<F, _>`, requires the `Representable` for `F`.
+ *
+ * @param rep - The instance of `Representable` for `F`.
+ * @returns The instance of `Representable` for `Cofree<F, _>`.
+ */
 export const representable = <F extends Hkt1, Rep>(
     rep: Representable<F, Rep>,
 ): Representable<Apply2Only<CofreeHkt, F>, Seq<Rep>> => {
