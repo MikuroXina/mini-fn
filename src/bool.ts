78e8de06d23a3eb2e18ab27084c5d5d4a3fface4
MikuroXina <ryosukadnak@gmail.com>
2023-03-01 15:23:32 +0900

    Add docs for Bool

--- src/bool.ts
+++ src/bool.ts
@@ -1,10 +1,16 @@
 import { fromEquality } from "./type-class/eq.js";
 import type { Monoid } from "./type-class/monoid.js";
 
+/**
+ * The instance of `Monoid` about logical AND operation.
+ */
 export const andMonoid: Monoid<boolean> = {
     identity: true,
     combine: (l, r) => l && r,
 };
+/**
+ * The instance of `Monoid` about logical OR operation.
+ */
 export const orMonoid: Monoid<boolean> = {
     identity: false,
     combine: (l, r) => l || r,
