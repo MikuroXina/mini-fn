5a145dd1aff8fe04c301843b9f150351170e1b05
MikuroXina <ryosukadnak@gmail.com>
2023-06-10 20:40:12 +0900

    Add trivialAbelianGroup

--- src/type-class/abelian-group.ts
+++ src/type-class/abelian-group.ts
@@ -1,4 +1,5 @@
 import type { Group } from "./group.js";
+import { semiGroupSymbol } from "./semi-group.js";
 
 export const abelSymbol = Symbol("ImplAbel");
 
@@ -8,3 +9,11 @@ export const abelSymbol = Symbol("ImplAbel");
 export interface AbelianGroup<G> extends Group<G> {
     readonly [abelSymbol]: true;
 }
+
+export const trivialAbelianGroup: AbelianGroup<[]> = {
+    combine: () => [],
+    identity: [],
+    invert: () => [],
+    [semiGroupSymbol]: true,
+    [abelSymbol]: true,
+};
