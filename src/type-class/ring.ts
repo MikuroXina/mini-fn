a2d9ad87ac7a53c0b4aa81059b73ef9f69bcf802
MikuroXina <ryosukadnak@gmail.com>
2023-06-10 22:13:30 +0900

    Fix typo

--- src/type-class/ring.ts
+++ src/type-class/ring.ts
@@ -10,7 +10,7 @@ import { type Monoid, trivialMonoid } from "./monoid.js";
  * - Identity on addition: for all `x`; `additive.combine(additive.identity, x)` equals to `x`.
  * - Inverse on addition: for all `x`; exists `y`; `additive.combine(x, y)` equals to `additive.identity`.
  * - Commutative on addition: for all `x` and `y`; `additive.combine(x, y)` equals to `additive.combine(y, x)`.
- * - Associative on multiplication: for all for all `x`, `y` and `z`; `multiplication.combine(multiplication.combine(x, y), z)` equals to `multiplication.combine(x, multiplication.combine(y, z))`
+ * - Associative on multiplication: for all `x`, `y` and `z`; `multiplication.combine(multiplication.combine(x, y), z)` equals to `multiplication.combine(x, multiplication.combine(y, z))`
  * - Identity on multiplication: for all `x`; `multiplication.combine(multiplication.identity, x)` equals to `multiplication.combine(x, multiplication.identity)` and `x`.
  * - Distributive: for all `x`, `y` and `z`; `multiplication.combine(x, additive.combine(y, z))` equals to `additive.combine(multiplication.combine(x, y), multiplication.combine(x, z))`
  */
