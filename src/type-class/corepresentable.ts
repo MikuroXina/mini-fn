9e6497a8fc85c2d920b8723785e4406cb388af4a
MikuroXina <ryosukadnak@gmail.com>
2023-08-25 00:36:10 +0900

    Fix GetCorep

--- src/type-class/corepresentable.ts
+++ src/type-class/corepresentable.ts
@@ -1,4 +1,4 @@
-import type { Get2, Hkt1, Instance } from "../hkt.js";
+import type { Get2, Hkt1 } from "../hkt.js";
 import type { Profunctor } from "./profunctor.js";
 
 /**
@@ -10,7 +10,7 @@ export interface HktCorep {
 
 export type ApplyCorep<F, R> = F extends Hkt1 ? F & { readonly corep: R } : never;
 
-export type GetCorep<F> = F extends HktCorep ? Instance<F["corep"]> : never;
+export type GetCorep<F> = F extends HktCorep ? F["corep"] : never;
 
 export interface Corepresentable<P> extends Profunctor<P> {
     readonly coindex: <A, B>(pab: Get2<P, A, B>) => (corep: GetCorep<P>) => B;
