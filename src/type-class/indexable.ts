c5d370d03c7d9247badc672464e41d95f0a1e263
MikuroXina <ryosukadnak@gmail.com>
2023-08-24 13:42:57 +0900

    Add Indexable

---
+++ src/type-class/indexable.ts
@@ -0,0 +1,17 @@
+import { constant, type Fn, type FnHkt } from "../func.js";
+import type { Get2 } from "../hkt.js";
+import type { Profunctor } from "./profunctor.js";
+
+export interface Indexable<I, P> extends Profunctor<P> {
+    readonly indexed: <A, B>(data: Get2<P, A, B>) => (index: I) => (a: A) => B;
+}
+
+export const fnIndexable = <I>(): Indexable<I, FnHkt> => ({
+    indexed: constant,
+    diMap:
+        <A, B>(f: (a: A) => B) =>
+        <C, D>(g: (c: C) => D) =>
+        (m: Fn<B, C>): Fn<A, D> =>
+        (a) =>
+            g(m(f(a))),
+});
