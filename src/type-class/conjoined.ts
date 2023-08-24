30e022d9fc536d60bcd62db8b2da8d434570d283
MikuroXina <ryosukadnak@gmail.com>
2023-08-24 22:27:04 +0900

    Extends Corepresentable for Conjoined

--- src/type-class/conjoined.ts
+++ src/type-class/conjoined.ts
@@ -1,8 +1,9 @@
 import type { Get1, Get2 } from "../hkt.js";
+import type { Corepresentable } from "./corepresentable.js";
 import type { Functor } from "./functor.js";
 import type { Profunctor } from "./profunctor.js";
 
-export interface Conjoined<P> extends Profunctor<P> {
+export interface Conjoined<P> extends Profunctor<P>, Corepresentable<P> {
     readonly distribute: <F>(
         f: Functor<F>,
     ) => <A, B>(pab: Get2<P, A, B>) => Get2<P, Get1<F, A>, Get1<F, B>>;
