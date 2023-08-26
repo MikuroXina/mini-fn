3c5ab3f25afaa57c95aee6c4d9af7167d0355231
MikuroXina <ryosukadnak@gmail.com>
2023-08-26 11:43:16 +0900

    Add indexedIndexable

--- src/type-class/indexed.ts
+++ src/type-class/indexed.ts
@@ -1,7 +1,18 @@
-import type { Hkt3 } from "src/hkt.js";
+import type { Apply3Only, Hkt3 } from "../hkt.js";
+import type { Indexable } from "./indexable.js";
 
 export type Indexed<I, A, B> = (i: I) => (a: A) => B;
 
 export interface IndexedHkt extends Hkt3 {
     readonly type: Indexed<this["arg3"], this["arg2"], this["arg1"]>;
 }
+export const diMap: <A, B>(
+    f: (a: A) => B,
+) => <C, D>(g: (c: C) => D) => <I>(m: Indexed<I, B, C>) => Indexed<I, A, D> =
+    (f) => (g) => (m) => (i) => (a) =>
+        g(m(i)(f(a)));
+
+export const indexedIndexable = <I>(): Indexable<I, Apply3Only<IndexedHkt, I>> => ({
+    indexed: (data) => data,
+    diMap,
+});
