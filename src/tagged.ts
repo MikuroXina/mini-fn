e0b8c26003d1a8752f2ee89bc5ba39bb99aee893
MikuroXina <ryosukadnak@gmail.com>
2023-08-27 21:06:54 +0900

    Add tagged and unTagged

--- src/tagged.ts
+++ src/tagged.ts
@@ -5,6 +5,10 @@ export interface Tagged<S, B> {
     readonly value: B;
 }
 
+export const tagged = <S, B>(value: B): Tagged<S, B> => ({ value });
+
+export const unTagged = <S, B>({ value }: Tagged<S, B>) => value;
+
 export interface TaggedHkt extends Hkt2 {
     readonly type: Tagged<this["arg2"], this["arg1"]>;
 }
