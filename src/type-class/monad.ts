05fb8f0254fc6cdc2157a06c5c762f3561894358
MikuroXina <ryosukadnak@gmail.com>
2023-09-19 18:19:22 +0900

    Add lift variants

--- src/type-class/monad.ts
+++ src/type-class/monad.ts
@@ -1,3 +1,4 @@
+import { doT } from "../cat.js";
 import { id, pipe } from "../func.js";
 import type { Get1 } from "../hkt.js";
 import { monad as idMonad } from "../identity.js";
@@ -10,6 +11,58 @@ export const flat = <S>(m: Monad<S>): (<A>(a: Get1<S, Get1<S, A>>) => Get1<S, A>
 
 export const liftM = <S>(m: Monad<S>): (<A, B>(f: (a: A) => B) => (ma: Get1<S, A>) => Get1<S, B>) =>
     m.map;
+
+export const liftM2 =
+    <S>(m: Monad<S>) =>
+    <A, B, C>(f: (a: A) => (b: B) => C) =>
+    (ma: Get1<S, A>) =>
+    (mb: Get1<S, B>): Get1<S, C> =>
+        doT(m)
+            .let("a", ma)
+            .let("b", mb)
+            .finish(({ a, b }) => f(a)(b));
+
+export const liftM3 =
+    <S>(m: Monad<S>) =>
+    <A, B, C, D>(f: (a: A) => (b: B) => (c: C) => D) =>
+    (ma: Get1<S, A>) =>
+    (mb: Get1<S, B>) =>
+    (mc: Get1<S, C>): Get1<S, D> =>
+        doT(m)
+            .let("a", ma)
+            .let("b", mb)
+            .let("c", mc)
+            .finish(({ a, b, c }) => f(a)(b)(c));
+
+export const liftM4 =
+    <S>(m: Monad<S>) =>
+    <A, B, C, D, E>(f: (a: A) => (b: B) => (c: C) => (d: D) => E) =>
+    (ma: Get1<S, A>) =>
+    (mb: Get1<S, B>) =>
+    (mc: Get1<S, C>) =>
+    (md: Get1<S, D>): Get1<S, E> =>
+        doT(m)
+            .let("a", ma)
+            .let("b", mb)
+            .let("c", mc)
+            .let("d", md)
+            .finish(({ a, b, c, d }) => f(a)(b)(c)(d));
+
+export const liftM5 =
+    <S>(m: Monad<S>) =>
+    <A, B, C, D, E, F>(f: (a: A) => (b: B) => (c: C) => (d: D) => (e: E) => F) =>
+    (ma: Get1<S, A>) =>
+    (mb: Get1<S, B>) =>
+    (mc: Get1<S, C>) =>
+    (md: Get1<S, D>) =>
+    (me: Get1<S, E>): Get1<S, F> =>
+        doT(m)
+            .let("a", ma)
+            .let("b", mb)
+            .let("c", mc)
+            .let("d", md)
+            .let("e", me)
+            .finish(({ a, b, c, d, e }) => f(a)(b)(c)(d)(e));
 
 export const begin = <S>(m: Monad<S>): Get1<S, object> => m.pure({});
 
