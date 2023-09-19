dbf91006ad9ad6ef57a456ec73ec6bb524de47dd
MikuroXina <ryosukadnak@gmail.com>
2023-09-19 18:46:41 +0900

    Add flatMap variants

--- src/type-class/monad.ts
+++ src/type-class/monad.ts
@@ -9,6 +9,48 @@ export interface Monad<S> extends Applicative<S>, FlatMap<S> {}
 
 export const flat = <S>(m: Monad<S>): (<A>(a: Get1<S, Get1<S, A>>) => Get1<S, A>) => m.flatMap(id);
 
+export const flatMap2 =
+    <S>(m: Monad<S>) =>
+    <A, B, C>(f: (a: A) => (mb: Get1<S, B>) => Get1<S, C>) =>
+    (ma: Get1<S, A>) =>
+    (mb: Get1<S, B>): Get1<S, C> =>
+        m.flatMap((a: A) => f(a)(mb))(ma);
+
+export const flatMap3 =
+    <S>(m: Monad<S>) =>
+    <A, B, C, D>(f: (a: A) => (mb: Get1<S, B>) => (mc: Get1<S, C>) => Get1<S, D>) =>
+    (ma: Get1<S, A>) =>
+    (mb: Get1<S, B>) =>
+    (mc: Get1<S, C>): Get1<S, D> =>
+        m.flatMap((a: A) => f(a)(mb)(mc))(ma);
+
+export const flatMap4 =
+    <S>(m: Monad<S>) =>
+    <A, B, C, D, E>(
+        f: (a: A) => (mb: Get1<S, B>) => (mc: Get1<S, C>) => (md: Get1<S, D>) => Get1<S, E>,
+    ) =>
+    (ma: Get1<S, A>) =>
+    (mb: Get1<S, B>) =>
+    (mc: Get1<S, C>) =>
+    (md: Get1<S, D>): Get1<S, E> =>
+        m.flatMap((a: A) => f(a)(mb)(mc)(md))(ma);
+
+export const flatMap5 =
+    <S>(m: Monad<S>) =>
+    <A, B, C, D, E, F>(
+        f: (
+            a: A,
+        ) => (
+            mb: Get1<S, B>,
+        ) => (mc: Get1<S, C>) => (md: Get1<S, D>) => (me: Get1<S, E>) => Get1<S, F>,
+    ) =>
+    (ma: Get1<S, A>) =>
+    (mb: Get1<S, B>) =>
+    (mc: Get1<S, C>) =>
+    (md: Get1<S, D>) =>
+    (me: Get1<S, E>): Get1<S, F> =>
+        m.flatMap((a: A) => f(a)(mb)(mc)(md)(me))(ma);
+
 export const liftM = <S>(m: Monad<S>): (<A, B>(f: (a: A) => B) => (ma: Get1<S, A>) => Get1<S, B>) =>
     m.map;
 
