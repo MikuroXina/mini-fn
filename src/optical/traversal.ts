52a4115c2330391745c7f195875988d03b270124
MikuroXina <ryosukadnak@gmail.com>
2023-09-30 02:19:28 +0900

    Improve Traversal definition

--- src/optical/traversal.ts
+++ src/optical/traversal.ts
@@ -1,25 +1,33 @@
-import { type Cont, type ContTHkt, monad } from "../cont.js";
-import type { Apply2Only, Apply3Only, Get1, Get2 } from "../hkt.js";
-import type { IdentityHkt } from "../identity.js";
+/**
+ * @packageDocumentation
+ * Traversing combinator for a data structure.
+ * ```text
+ *    T<A> -----|--[ get ]-> A
+ *              V
+ * F<T<B>> <-[ set ]-------- F<B>
+ * ```
+ */
+
+import { type Cont, type ContHkt, map, monad } from "../cont.js";
+import type { Apply2Only, Get1, Get2 } from "../hkt.js";
 import type { Optic } from "../optical.js";
-import type { Bitraversable } from "../type-class/bitraversable.js";
-import { type Traversable } from "../type-class/traversable.js";
+import type { Applicative } from "../type-class/applicative.js";
+import { bisequenceA, type Bitraversable } from "../type-class/bitraversable.js";
+import { sequenceA, type Traversable } from "../type-class/traversable.js";
 
-export type Traversal<T, A, B> = Optic<Get1<T, A>, Get1<T, B>, A, B>;
-export type Bitraversal<T, A, B> = Optic<Get2<T, A, A>, Get2<T, B, B>, A, B>;
+export type Traversal<T, F, A, B> = Optic<Get1<T, A>, Get1<F, Get1<T, B>>, A, Get1<F, B>>;
+export type Bitraversal<T, F, A, B> = Optic<Get2<T, A, A>, Get1<F, Get2<T, B, B>>, A, Get1<F, B>>;
 
-export const traversed =
-    <T, A, B>(tra: Traversable<T>): Traversal<T, A, B> =>
-    <R>(next: (sending: A) => Cont<R, B>) =>
+export const newTraversal =
+    <T, F, A, B>(tra: Traversable<T>, app: Applicative<F>): Traversal<T, F, A, B> =>
+    <R>(next: (sending: A) => Cont<R, Get1<F, B>>) =>
     (received) =>
-        tra.traverse<Apply3Only<ContTHkt, R> & Apply2Only<ContTHkt, IdentityHkt>>(monad())(next)(
-            received,
+        map(sequenceA(tra, app))(tra.traverse<Apply2Only<ContHkt, R>>(monad())(next)(received));
+
+export const newBitraversal =
+    <T, F, A, B>(tra: Bitraversable<T>, app: Applicative<F>): Bitraversal<T, F, A, B> =>
+    <R>(next: (sending: A) => Cont<R, Get1<F, B>>) =>
+    (received: Get2<T, A, A>): Cont<R, Get1<F, Get2<T, B, B>>> =>
+        map(bisequenceA(tra, app)<B, B>)(
+            tra.bitraverse<Apply2Only<ContHkt, R>>(monad())(next)(next)(received),
         );
-
-export const both =
-    <T, A, B>(bi: Bitraversable<T>): Bitraversal<T, A, B> =>
-    <R>(next: (sending: A) => Cont<R, B>) =>
-    (received) =>
-        bi.bitraverse<Apply3Only<ContTHkt, R> & Apply2Only<ContTHkt, IdentityHkt>>(monad())(next)(
-            next,
-        )(received);
