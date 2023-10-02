0b5ba60e754fa11f3698b67ff69ddc59a16afc0a
MikuroXina <ryosukadnak@gmail.com>
2023-10-02 21:30:59 +0900

    Improve Getter

--- src/optical/getter.ts
+++ src/optical/getter.ts
@@ -8,14 +8,19 @@
  * ```
  */
 
-import type { Optic } from "../optical.js";
-
-export type Getter<S, A, T> = Optic<S, T, A, T>;
+export interface Getter<S, A> {
+    <T>(over: (a: A) => T): (s: S) => T;
+}
 
 export const newGetter =
-    <S, A, T>(getter: (s: S) => A): Getter<S, A, T> =>
-    (next) =>
-    (received) =>
-    (callback) => {
-        return next(getter(received))(callback);
-    };
+    <S, A>(getter: (s: S) => A): Getter<S, A> =>
+    (over) =>
+    (s) =>
+        over(getter(s));
+
+export type Getting<R, S, A> = (over: (a: A) => R) => (s: S) => R;
+
+export const gets =
+    <S>(s: S) =>
+    <A>(l: Getting<A, S, A>): A =>
+        l((a) => a)(s);
