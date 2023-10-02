30b79dce05cc9872f51a4d35b2027d1d8b801d1a
MikuroXina <ryosukadnak@gmail.com>
2023-10-03 00:57:02 +0900

    Enforce features for type classes

--- src/optical/setter.ts
+++ src/optical/setter.ts
@@ -8,9 +8,12 @@
  * ```
  */
 
-import type { Get1 } from "../hkt.js";
+import type { Get1, Get2 } from "../hkt.js";
+import { type Option, some } from "../option.js";
 import type { Functor } from "../type-class/functor.js";
 import type { Monad } from "../type-class/monad.js";
+import { leftMap, type Profunctor } from "../type-class/profunctor.js";
+import type { Contravariant } from "../type-class/variance.js";
 
 /**
  * `Setter` is a `Optic` which transform `S` into `T` with function `A` to `B`.
@@ -41,6 +44,11 @@ export const set =
 export const sets = <S, T, A, B>(f: (over: (a: A) => B) => (s: S) => T): Setter<S, T, A, B> => f;
 
 /**
+ * Extracts a converting function from `Setter`. It is equivalent to an identity function.
+ */
+export const over = <S, T, A, B>(l: Setter<S, T, A, B>): ((over: (a: A) => B) => (s: S) => T) => l;
+
+/**
  * Replaces the value at entry with the value.
  *
  * @param lens - The lens focused to.
@@ -53,7 +61,43 @@ export const replace =
         lens(() => newValue);
 
 /**
- * Modifies data contained by `Functor` as a terminal.
+ * Replaces the optional value at entry with the some value.
+ *
+ * @param lens - The lens focused to.
+ * @param newValue - The value will be inserted.
+ * @returns The transformation computation.
+ */
+export const replaceSome =
+    <S, T, A, B>(lens: Setter<S, T, A, Option<B>>) =>
+    (newValue: B): ((s: S) => T) =>
+        lens(() => some(newValue));
+
+/**
+ * Replaces the value at entry with the value, and keeps the inserted item.
+ *
+ * @param lens - The lens focused to.
+ * @param newValue - The value will be inserted.
+ * @returns The transformation computation.
+ */
+export const replaceThrough =
+    <S, T, A, B>(lens: Setter<S, T, A, B>) =>
+    (newValue: B) =>
+    (s: S): [B, T] => [newValue, replace(lens)(newValue)(s)];
+
+/**
+ * Replaces the optional value at entry with the some value, and keeps the inserted item.
+ *
+ * @param lens - The lens focused to.
+ * @param newValue - The value will be inserted.
+ * @returns The transformation computation.
+ */
+export const replaceSomeThrough =
+    <S, T, A, B>(lens: Setter<S, T, A, Option<B>>) =>
+    (newValue: B) =>
+    (s: S): [B, T] => [newValue, replaceSome(lens)(newValue)(s)];
+
+/**
+ * Modifies data contained by `Functor`.
  *
  * @param mapper - The function to map the data going.
  * @returns The mapping optic.
@@ -61,9 +105,27 @@ export const replace =
 export const setF = <F, S, T>(f: Functor<F>): Setter<Get1<F, S>, Get1<F, T>, S, T> => f.map;
 
 /**
- * Modifies data contained by `Monad` as a terminal.
+ * Modifies data contained by `Monad`.
  *
  * @param mapper - The function to map the data going.
  * @returns The mapping optic.
  */
 export const setM = <M, S, T>(m: Monad<M>): Setter<Get1<M, S>, Get1<M, T>, S, T> => m.map;
+
+/**
+ * Modifies data contained by `Contravariant`.
+ *
+ * @param mapper - The function to map the data going.
+ * @returns The mapping optic.
+ */
+export const setC = <F, S, T>(c: Contravariant<F>): Setter<Get1<F, S>, Get1<F, T>, T, S> =>
+    c.contraMap;
+
+/**
+ * Modifies data contained by `Profunctor`.
+ *
+ * @param mapper - The function to map the data going.
+ * @returns The mapping optic.
+ */
+export const setP: <P, S, T, R>(p: Profunctor<P>) => Setter<Get2<P, S, R>, Get2<P, T, R>, T, S> =
+    leftMap;
