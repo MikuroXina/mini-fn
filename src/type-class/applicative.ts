025676c93068fd97afaf467032be22de80ad286f
MikuroXina <ryosukadnak@gmail.com>
2024-05-13 08:15:58 +0900

    Add applicative laws as docs

--- src/type-class/applicative.ts
+++ src/type-class/applicative.ts
@@ -5,6 +5,16 @@ import type { Monoid } from "./monoid.ts";
 import type { Pure } from "./pure.ts";
 import { semiGroupSymbol } from "./semi-group.ts";
 
+/**
+ * A functor with application. It can combine sequence computations with `apply` or `liftA2` function.
+ *
+ * All instances of the applicative `a` must satisfy the following laws:
+ *
+ * - Identity: For all `x`; `a.apply(a.pure((i) => i))(x)` equals to `x`,
+ * - Composition: For all `x`, `y` and `z`; `a.apply((a.apply(a.apply((f) => (g) => (i) => f(g(i)))(x))(y)))(z)` equals to `a.apply(x)(a.apply(y)(z))`,
+ * - Homomorphism: For all `f` and `x`; `a.apply(a.pure(f))(a.pure(x))` equals to `a.pure(f(x))`,
+ * - Interchange: For all `f` and `x`; `a.apply(f)(a.pure(x))` equals to `a.apply(a.pure((i) => i(x)))(f)`.
+ */
 export interface Applicative<S> extends Apply<S>, Pure<S> {}
 
 export const makeMonoid = <S>(
