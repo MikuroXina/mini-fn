b863a6f1ddf14dc1cb4c8813d4ea5f5f666ba338
MikuroXina <ryosukadnak@gmail.com>
2024-05-18 07:04:37 +0900

    Fix identity law

--- src/type-class/traversable.ts
+++ src/type-class/traversable.ts
@@ -10,7 +10,7 @@ import type { Monad } from "./monad.ts";
  * All instances of the traversable functor `tra` must satisfy the following laws:
  *
  * - Naturality: For all `t`, `f` and `x`; `t(tra.traverse(app)(f)(x))` equals to `tra.traverse((item) => t(f(item)))(x)`,
- * - Identity: For all `f`; `tra.traverse(Identity.applicative)(f)` equals to `f`,
+ * - Identity: For all x; `tra.traverse(Identity.applicative)((a) => a)(x)` equals to `x`,
  * - Composition: For all `f`, `g`, `x` and composed applicative functor `app` for `Compose<F, G, _>`; `tra.traverse(app)((item) => app.map(g)(f(item)))(x)` equals to `app.map(tra.traverse(app)(g))(tra.traverse(app)(f)(x))`.
  */
 export interface Traversable<T> extends Functor<T>, Foldable<T> {
