0517b4d8faa58353a231c85648abdc3ab0ee8720
MikuroXina <mikuroxina@gmail.com>
2024-07-24 15:13:17 +0900

    feat: Add distributive for Compose

--- src/compose.ts
+++ src/compose.ts
@@ -7,6 +7,7 @@
 
 import type { Apply2Only, Apply3Only, Get1, Hkt3 } from "./hkt.ts";
 import { type Applicative, liftA2 } from "./type-class/applicative.ts";
+import { collect, type Distributive } from "./type-class/distributive.ts";
 import type { Foldable } from "./type-class/foldable.ts";
 import type { Functor } from "./type-class/functor.ts";
 import type { Traversable } from "./type-class/traversable.ts";
@@ -20,6 +21,17 @@ export interface ComposeHkt extends Hkt3 {
     readonly type: Compose<this["arg3"], this["arg2"], this["arg1"]>;
 }
 
+export const newCompose = <F, G, T>(
+    fgt: Get1<F, Get1<G, T>>,
+): Compose<F, G, T> => fgt;
+export const getCompose = <F, G, T>(c: Compose<F, G, T>): Get1<F, Get1<G, T>> =>
+    c;
+
+export const map =
+    <F, G>(f: Functor<F>, g: Functor<G>) =>
+    <T, U>(fn: (t: T) => U): (t: Compose<F, G, T>) => Compose<F, G, U> =>
+        f.map(g.map(fn));
+
 /**
  * Composes the two functors into a new one.
  *
@@ -30,7 +42,7 @@ export interface ComposeHkt extends Hkt3 {
 export const functor =
     <F>(f: Functor<F>) =>
     <G>(g: Functor<G>): Functor<Apply2Only<Apply3Only<ComposeHkt, F>, G>> => ({
-        map: (fn) => f.map(g.map(fn)),
+        map: map(f, g),
     });
 
 /**
@@ -45,7 +57,7 @@ export const applicative = <F>(f: Applicative<F>) =>
     g: Applicative<G>,
 ): Applicative<Apply2Only<Apply3Only<ComposeHkt, F>, G>> => ({
     pure: (t) => f.pure(g.pure(t)),
-    map: (fn) => f.map(g.map(fn)),
+    map: map(f, g),
     apply: liftA2(f)(g.apply),
 });
 
@@ -75,8 +87,21 @@ export const traversable = <F>(f: Traversable<F>) =>
 <G>(
     g: Traversable<G>,
 ): Traversable<Apply2Only<Apply3Only<ComposeHkt, F>, G>> => ({
-    map: (fn) => f.map(g.map(fn)),
+    map: map(f, g),
     foldR: <A, B>(folder: (next: A) => (acc: B) => B) =>
         f.foldR((ga: Get1<G, A>) => (acc: B) => g.foldR(folder)(acc)(ga)),
     traverse: (app) => (visitor) => f.traverse(app)(g.traverse(app)(visitor)),
+});
+
+export const distributive = <F, G>(
+    distributiveF: Distributive<F>,
+    distributiveG: Distributive<G>,
+): Distributive<Apply2Only<Apply3Only<ComposeHkt, F>, G>> => ({
+    map: map(distributiveF, distributiveG),
+    distribute:
+        <S>(functor: Functor<S>) =>
+        <T>(fga: Get1<S, Compose<F, G, T>>): Compose<F, G, Get1<S, T>> =>
+            distributiveF.map(distributiveG.distribute(functor))(
+                collect(distributiveF)(functor)(getCompose<F, G, T>)(fga),
+            ),
 });
