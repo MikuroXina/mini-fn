75564ef841a5f7a0b5b00ccc67e0b8d92d877e6c
MikuroXina <mikuroxina@gmail.com>
2024-08-15 12:55:18 +0900

    test: Add test for traversed

--- src/optical/traversal.test.ts
+++ src/optical/traversal.test.ts
@@ -1,7 +1,8 @@
 import { overCat } from "../optical.ts";
 import { bitraversable, type Tuple } from "../tuple.ts";
-import { both } from "./traversal.ts";
+import { both, traversed } from "./traversal.ts";
 import { assertEquals } from "../../deps.ts";
+import { Array } from "../../mod.ts";
 
 Deno.test("get length of string for each element of tuple", () => {
     const tuple: Tuple<string, string> = ["hello", "world"];
@@ -10,5 +11,15 @@ Deno.test("get length of string for each element of tuple", () => {
             .on(both(bitraversable))
             .from(tuple),
         [5, 5],
+    );
+});
+
+Deno.test("traversed", () => {
+    const values = ["hey", "what", "are", "you", "doing"];
+    assertEquals(
+        overCat((s: string) => s.length)
+            .on(traversed(Array.traversable))
+            .from(values),
+        [3, 4, 3, 3, 5],
     );
 });
