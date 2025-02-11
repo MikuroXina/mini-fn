7a847f394cdb7944caa4a1f6866b76bb1fd3716a
MikuroXina <10331164+MikuroXina@users.noreply.github.com>
2025-02-12 02:33:29 +0900

    Add mapMutRef

--- src/mut.ts
+++ src/mut.ts
@@ -1,6 +1,8 @@
 /**
  * This module provides the mutable reference in the state transformer monad. It allows you to program destructive operations safely.
  *
+ * `Mut<S, A>` lets you use in-place operations including variable scope. Type argument `S` means a thread scope where is belonged, and `A` means the type of computation result. It manages computation as a lazily evaluated function, so you can combine operations using mutability freely.
+ *
  * @packageDocumentation
  * @module
  */
@@ -157,7 +159,7 @@ export const functor = <S>(): Functor<Apply2Only<MutHkt, S>> => ({
  * @returns The `Applicative` instance for `Mut<S, _>`.
  */
 export const applicative = <S>(): Applicative<Apply2Only<MutHkt, S>> => ({
-    map: mapMut,
+    ...functor(),
     pure: pureMut,
     apply: applyMut,
 });
@@ -197,6 +199,17 @@ export const newMutRef = <S, A>(value: A): Mut<S, MutRef<S, A>> => (thread) =>
  */
 export const readMutRef = <S, A>(ref: MutRef<S, A>): Mut<S, A> =>
     readThreadVar(ref);
+
+/**
+ * Reads a value from the reference and converts by `fn`. It throws on the referent value was not initialized or dropped.
+ *
+ * @param fn - A mapping function.
+ * @param ref - A target to read.
+ * @returns The reading and mapping operation which results `B`.
+ */
+export const mapMutRef =
+    <A, B>(fn: (a: A) => B) => <S>(ref: MutRef<S, A>): Mut<S, B> =>
+        mapMut(fn)(readMutRef(ref));
 
 /**
  * Writes a value into the reference in a scope of `Mut` environment.
