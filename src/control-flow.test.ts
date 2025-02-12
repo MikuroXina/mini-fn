d75688d7089c654698a8f816a3d69d8686fad31a
Mikuroさいな <10331164+MikuroXina@users.noreply.github.com>
2025-02-12 14:25:01 +0900

    feat: Add applicative for ControlFlow and Enforce tests (#273)

--- src/control-flow.test.ts
+++ src/control-flow.test.ts
@@ -1,6 +1,7 @@
 import { assertEquals } from "../deps.ts";
 import { Array, Compose } from "../mod.ts";
 import {
+    applicative,
     apply,
     biMap,
     breakValue,
@@ -8,6 +9,7 @@ import {
     type ControlFlow,
     dec,
     enc,
+    eq,
     flatten,
     foldR,
     functor,
@@ -18,7 +20,9 @@ import {
     monad,
     newBreak,
     newContinue,
+    partialEqUnary,
     traversable,
+    traversableMonad,
 } from "./control-flow.ts";
 import { applicative as applicativeIdentity } from "./identity.ts";
 import {
@@ -36,6 +40,7 @@ import {
     runCode,
     runDecoder,
 } from "./serial.ts";
+import { eqSymbol, stringEq } from "./type-class/eq.ts";
 
 const cases = [newContinue("foo"), newBreak("bar")] as const;
 
@@ -54,6 +59,73 @@ Deno.test("extract", () => {
     ]);
 });
 
+Deno.test("equality", () => {
+    const equality = eq({
+        eq: (l: bigint, r: bigint) => l === r,
+        [eqSymbol]: true,
+    }, stringEq);
+    // symmetric
+    for (let x = -100n; x <= 100n; ++x) {
+        for (const y of ["foo", "bar", "", "hoge"]) {
+            assertEquals(equality.eq(newBreak(x), newBreak(x)), true);
+            assertEquals(equality.eq(newContinue(y), newContinue(y)), true);
+            assertEquals(equality.eq(newBreak(x), newContinue(y)), false);
+            assertEquals(equality.eq(newContinue(y), newBreak(x)), false);
+        }
+    }
+
+    // transitive
+    for (let x = -100n; x <= 100n; ++x) {
+        for (const y of ["foo", "bar", "", "hoge"]) {
+            for (let z = -200n; z <= 200n; ++z) {
+                assertEquals(
+                    equality.eq(newBreak(x), newContinue(y)) &&
+                        equality.eq(newContinue(y), newBreak(z)),
+                    false,
+                );
+                assertEquals(
+                    equality.eq(newContinue(y), newBreak(x)) &&
+                        equality.eq(newBreak(x), newBreak(z)),
+                    false,
+                );
+            }
+        }
+    }
+});
+
+Deno.test("partial equality unary", () => {
+    const equalityUnary = partialEqUnary(stringEq);
+    const equality = equalityUnary.liftEq((l: bigint, r: bigint) => l === r);
+
+    // symmetric
+    for (const x of ["foo", "bar", "", "hoge"]) {
+        for (let y = -100n; y <= 100n; ++y) {
+            assertEquals(equality(newBreak(x), newBreak(x)), true);
+            assertEquals(equality(newContinue(y), newContinue(y)), true);
+            assertEquals(equality(newBreak(x), newContinue(y)), false);
+            assertEquals(equality(newContinue(y), newBreak(x)), false);
+        }
+    }
+
+    // transitive
+    for (const x of ["foo", "bar", "", "hoge"]) {
+        for (let y = -100n; y <= 100n; ++y) {
+            for (let z = -200n; z <= 200n; ++z) {
+                assertEquals(
+                    equality(newBreak(x), newContinue(y)) &&
+                        equality(newContinue(y), newContinue(z)),
+                    false,
+                );
+                assertEquals(
+                    equality(newContinue(y), newBreak(x)) &&
+                        equality(newBreak(x), newContinue(z)),
+                    false,
+                );
+            }
+        }
+    }
+});
+
 Deno.test("map", () => {
     const actual = cases.map((
         c,
@@ -65,6 +137,17 @@ Deno.test("map", () => {
         [newContinue("FOO"), newContinue("foo")],
         [newBreak("bar"), newBreak("BAR")],
     ]);
+});
+
+Deno.test("apply", () => {
+    const mapper = newContinue((x: string) => x + "!") as ControlFlow<
+        never[],
+        (x: string) => string
+    >;
+    assertEquals(apply(mapper)(newContinue("foo")), newContinue("foo!"));
+    assertEquals(apply(mapper)(newBreak([])), newBreak([]));
+    assertEquals(apply(newBreak([]))(newContinue("foo")), newBreak([]));
+    assertEquals(apply(newBreak([]))(newBreak([])), newBreak([]));
 });
 
 Deno.test("biMap", () => {
@@ -99,28 +182,30 @@ Deno.test("functor laws", () => {
 });
 
 Deno.test("applicative laws", () => {
-    const a = monad<never[]>();
-    const data = newContinue("2");
-    // identity
-    assertEquals(apply(a.pure((x: string) => x))(data), data);
-
-    // composition
+    const a = applicative<string>();
     const strLen = a.pure((x: string) => x.length);
     const question = a.pure((x: string) => x + "?");
-    assertEquals(
-        apply(
+
+    for (const data of cases) {
+        // identity
+        assertEquals(apply(a.pure((x: string) => x))(data), data);
+
+        // composition
+        assertEquals(
             apply(
                 apply(
-                    a.pure(
-                        (f: (x: string) => number) =>
-                        (g: (x: string) => string) =>
-                        (i: string) => f(g(i)),
-                    ),
-                )(strLen),
-            )(question),
-        )(data),
-        apply(strLen)(apply(question)(data)),
-    );
+                    apply(
+                        a.pure(
+                            (f: (x: string) => number) =>
+                            (g: (x: string) => string) =>
+                            (i: string) => f(g(i)),
+                        ),
+                    )(strLen),
+                )(question),
+            )(data),
+            apply(strLen)(apply(question)(data)),
+        );
+    }
 
     // homomorphism
     assertEquals(
@@ -197,6 +282,66 @@ Deno.test("traversable laws", () => {
 
     // identity
     for (const c of cases) {
+        assertEquals(t.traverse(applicativeIdentity)((x: string) => x)(c), c);
+    }
+
+    // composition
+    const firstCh = (x: string): Option<string> =>
+        x.length > 0 ? some(x.charAt(0)) : none();
+    assertEquals(
+        t.traverse(Compose.applicative(Array.applicative)(applicativeOption))(
+            (x: string) => Array.map(firstCh)(dup(x)),
+        )(data),
+        Array.map(t.traverse(applicativeOption)(firstCh))(
+            t.traverse(Array.applicative)(dup)(data),
+        ),
+    );
+});
+
+Deno.test("traversable monad laws", () => {
+    const t = traversableMonad<number>();
+
+    const continuing = (x: string): ControlFlow<number, string> =>
+        newContinue(x);
+    const breaking = (x: string): ControlFlow<number, string> =>
+        newBreak(x.length);
+
+    // left identity
+    for (const c of [continuing, breaking]) {
+        assertEquals(t.flatMap(c)(t.pure("baz")), c("baz"));
+    }
+
+    // right identity
+    assertEquals(t.flatMap(t.pure)(newContinue("a")), newContinue("a"));
+    assertEquals(t.flatMap(t.pure)(newBreak(2)), newBreak(2));
+
+    // associativity
+    for (const data of [newContinue("a"), newBreak(2)]) {
+        assertEquals(
+            t.flatMap(breaking)(t.flatMap(continuing)(data)),
+            t.flatMap((x: string) => t.flatMap(breaking)(continuing(x)))(data),
+        );
+        assertEquals(
+            t.flatMap(continuing)(t.flatMap(breaking)(data)),
+            t.flatMap((x: string) => t.flatMap(continuing)(breaking(x)))(data),
+        );
+    }
+
+    // naturality
+    const first = <T>(
+        x: readonly T[],
+    ): Option<T> => 0 in x ? some(x[0]) : none();
+    const dup = (x: string): readonly string[] => [x + "0", x + "1"];
+    const data = newContinue("fever");
+    assertEquals(
+        first(t.traverse(Array.applicative)(dup)(data)),
+        t.traverse(applicativeOption)((item: string) => first(dup(item)))(
+            data,
+        ),
+    );
+
+    // identity
+    for (const c of [newContinue("foo"), newBreak(6)]) {
         assertEquals(t.traverse(applicativeIdentity)((x: string) => x)(c), c);
     }
 
--- src/control-flow.ts
+++ src/control-flow.ts
@@ -31,6 +31,13 @@ const continueSymbol = Symbol("ControlFlowContinue");
  * A variant telling that an operation should go on with a value of type `C`.
  */
 export type Continue<C> = readonly [typeof continueSymbol, C];
+
+/**
+ * Creates an object that represents the code flow to be continued.
+ *
+ * @param c - Continuation value.
+ * @returns A new `ControlFlow` object.
+ */
 export const newContinue = <C>(c: C): Continue<C> => [continueSymbol, c];
 
 const breakSymbol = Symbol("ControlFlowBreak");
@@ -38,6 +45,12 @@ const breakSymbol = Symbol("ControlFlowBreak");
  * A variant telling that an operation should exit early with a value of type `B`.
  */
 export type Break<B> = readonly [typeof breakSymbol, B];
+
+/**
+ * Creates an object that represents the code flow to be discontinued.
+ * @param b - Break value.
+ * @returns A new `ControlFlow` object.
+ */
 export const newBreak = <B>(b: B): Break<B> => [breakSymbol, b];
 
 /**
@@ -45,6 +58,13 @@ export const newBreak = <B>(b: B): Break<B> => [breakSymbol, b];
  */
 export type ControlFlow<B, C = never[]> = Continue<C> | Break<B>;
 
+/**
+ * Constructs a partial equality of `ControlFlow` with equalities for its element.
+ *
+ * @param equalityB - Equality for break values.
+ * @param equalityC - Equality for continue values.
+ * @returns A composite equality.
+ */
 export const partialEq = <B, C>(
     equalityB: PartialEq<B>,
     equalityC: PartialEq<C>,
@@ -54,17 +74,27 @@ export const partialEq = <B, C>(
             ? equalityB.eq(l[1], r[1])
             : isContinue(l) && isContinue(r) && equalityC.eq(l[1], r[1]),
 });
+/**
+ * Constructs a total equality of `ControlFlow` with equalities for its element.
+ *
+ * @param equalityB - Equality for break values.
+ * @param equalityC - Equality for continue values.
+ * @returns A composite equality.
+ */
 export const eq = <B, C>(
     equalityB: Eq<B>,
     equalityC: Eq<C>,
 ): Eq<ControlFlow<B, C>> => ({
-    eq: (l, r) =>
-        isBreak(l) && isBreak(r)
-            ? equalityB.eq(l[1], r[1])
-            : isContinue(l) && isContinue(r) && equalityC.eq(l[1], r[1]),
+    ...partialEq(equalityB, equalityC),
     [eqSymbol]: true,
 });
 
+/**
+ * Constructs an utility to make a new equality of `ControlFlow`.
+ *
+ * @param equalityB - Equality for break values.
+ * @returns An equality factory.
+ */
 export const partialEqUnary = <B>(
     equalityB: PartialEq<B>,
 ): PartialEqUnary<Apply2Only<ControlFlowHkt, B>> => ({
@@ -76,25 +106,71 @@ export const partialEqUnary = <B>(
                 : isContinue(l) && isContinue(r) && equality(l[1], r[1]),
 });
 
+/**
+ * Returns whether the `ControlFlow` object is a `Continue`.
+ *
+ * @param cf - To be determined.
+ * @returns Whether the `ControlFlow` object is a `Continue`.
+ */
 export const isContinue = <B, C>(cf: ControlFlow<B, C>): cf is Continue<C> =>
     cf[0] === continueSymbol;
+/**
+ * Returns whether the `ControlFlow` object is a `Break`.
+ *
+ * @param cf - To be determined.
+ * @returns Whether the `ControlFlow` object is a `Break`.
+ */
 export const isBreak = <B, C>(cf: ControlFlow<B, C>): cf is Break<B> =>
     cf[0] === breakSymbol;
 
+/**
+ * Extracts the continue value in the `ControlFlow` object if it is a `Continue`, or `None` otherwise.
+ *
+ * @param cf - To be extracted.
+ * @returns The continue value in `Some` if it is a `Continue`, or `None` otherwise.
+ */
 export const continueValue = <B, C>(cf: ControlFlow<B, C>): Option<C> =>
     isContinue(cf) ? some(cf[1]) : none();
+/**
+ * Extracts the break value in the `ControlFlow` object if it is a `Break`, or `None` otherwise.
+ *
+ * @param cf - To be extracted.
+ * @returns The break value in `Some` if it is a `Break`, or `None` otherwise.
+ */
 export const breakValue = <B, C>(cf: ControlFlow<B, C>): Option<B> =>
     isBreak(cf) ? some(cf[1]) : none();
 
+/**
+ * Transforms the continue value in the `ControlFlow` object if only it is a `Continue`.
+ *
+ * @param mapper - Function to transform the continue value.
+ * @param cf - To be transformed,
+ * @returns The transformed object.
+ */
 export const mapContinue =
     <C1, C2>(mapper: (c: C1) => C2) =>
     <B>(cf: ControlFlow<B, C1>): ControlFlow<B, C2> =>
         isContinue(cf) ? newContinue(mapper(cf[1])) : cf;
+/**
+ * Transforms the break value in the `ControlFlow` object if only it is a `Break`.
+ *
+ * @param mapper - Function to transform the break value.
+ * @param cf - To be transformed,
+ * @returns The transformed object.
+ */
 export const mapBreak =
     <B1, B2>(mapper: (b: B1) => B2) =>
     <C>(cf: ControlFlow<B1, C>): ControlFlow<B2, C> =>
         isBreak(cf) ? newBreak(mapper(cf[1])) : cf;
 
+/**
+ * Transforms the internal value in the `ControlFlow` object.
+ *
+ * @param breakMapper - Function to transform the break value.
+ * @param continueMapper - Function to transform the continue value.
+ * @param cf - To be transformed,
+ * @returns The transformed object.
+ */
 export const biMap =
     <B1, B2>(breakMapper: (b: B1) => B2) =>
     <C1, C2>(continueMapper: (c: C1) => C2) =>
@@ -103,10 +179,23 @@ export const biMap =
             ? newBreak(breakMapper(cf[1]))
             : newContinue(continueMapper(cf[1]));
 
+/**
+ * Flattens the nested `ControlFlow` object. Types of break element must be same.
+ *
+ * @param cfCf - The nested item.
+ * @returns A flattened one.
+ */
 export const flatten = <B, C>(
     cfCf: ControlFlow<B, ControlFlow<B, C>>,
 ): ControlFlow<B, C> => isContinue(cfCf) ? cfCf[1] : cfCf;
 
+/**
+ * Applies the function in `ControlFlow` to the continue value of another `ControlFlow`.
+ *
+ * @param cfFn - Tp be applied.
+ * @param cf - To be transformed,
+ * @returns An applied one.
+ */
 export const apply =
     <B, C1, C2>(cfFn: ControlFlow<B, (c: C1) => C2>) =>
     (cf: ControlFlow<B, C1>): ControlFlow<B, C2> =>
@@ -114,17 +203,40 @@ export const apply =
             ? (isContinue(cf) ? newContinue(cfFn[1](cf[1])) : cf)
             : cfFn;
 
+/**
+ * Maps the continue value of `ControlFlow` object by the specified function, and then flattens it.
+ *
+ * @param fn - Function to transform the continue value.
+ * @param cf - To be transformed,
+ * @returns A transformed one.
+ */
 export const flatMap =
     <B, C1, C2>(fn: (c: C1) => ControlFlow<B, C2>) =>
     (cf: ControlFlow<B, C1>): ControlFlow<B, C2> =>
         isContinue(cf) ? fn(cf[1]) : cf;
 
+/**
+ * Folds the internal value into an accumulate value. It extracts only a continue value of the `ControlFlow` object.
+ *
+ * @param folder - Function to fold the continue value item and the accumulate.
+ * @param init - Initial value of the accumulate.
+ * @param data - Source to fold.
+ * @returns The folded accumulate value.
+ */
 export const foldR =
     <C, X>(folder: (next: C) => (acc: X) => X) =>
     (init: X) =>
     <B>(data: ControlFlow<B, C>): X =>
         isContinue(data) ? folder(data[1])(init) : init;
 
+/**
+ * Traverses the internal value into a new container value. It extracts only a continue value of the `ControlFlow` object.
+ *
+ * @param app - The `Applicative` instance of `F`.
+ * @param visitor - Function to transform the continue value item into another container value.
+ * @param data - Source to traverse.
+ * @returns The traversed value in the container `F`.
+ */
 export const traverse = <F>(
     app: Applicative<F>,
 ) =>
@@ -140,10 +252,27 @@ export interface ControlFlowHkt extends Hkt2 {
     readonly type: ControlFlow<this["arg2"], this["arg1"]>;
 }
 
+/**
+ * The `Functor` instance for `ControlFlow<B, _>`.
+ */
 export const functor = <B>(): Functor<Apply2Only<ControlFlowHkt, B>> => ({
     map: mapContinue,
 });
 
+/**
+ * The `Applicative` instance for `ControlFlow<B, _>`.
+ */
+export const applicative = <B>(): Applicative<
+    Apply2Only<ControlFlowHkt, B>
+> => ({
+    pure: newContinue,
+    map: mapContinue,
+    apply,
+});
+
+/**
+ * The `Monad` instance for `ControlFlow<B, _>`.
+ */
 export const monad = <B>(): Monad<Apply2Only<ControlFlowHkt, B>> => ({
     pure: newContinue,
     map: mapContinue,
@@ -151,6 +280,9 @@ export const monad = <B>(): Monad<Apply2Only<ControlFlowHkt, B>> => ({
     flatMap,
 });
 
+/**
+ * The `Traversable` instance for `ControlFlow<B, _>`.
+ */
 export const traversable = <B>(): Traversable<
     Apply2Only<ControlFlowHkt, B>
 > => ({
@@ -159,6 +291,9 @@ export const traversable = <B>(): Traversable<
     traverse,
 });
 
+/**
+ * The `TraversableMonad` instance for `ControlFlow<B, _>`.
+ */
 export const traversableMonad = <B>(): TraversableMonad<
     Apply2Only<ControlFlowHkt, B>
 > => ({
@@ -166,8 +301,18 @@ export const traversableMonad = <B>(): TraversableMonad<
     ...traversable(),
 });
 
+/**
+ * The `Bifunctor` instance for `ControlFlow<_, _>`.
+ */
 export const bifunctor: Bifunctor<ControlFlowHkt> = { biMap };
 
+/**
+ * Generates an `Encoder` for a `ControlFlow` object.
+ *
+ * @param encB - An encoder for break value.
+ * @param encC - An encoder for continue value.
+ * @returns A composite encoder.
+ */
 export const enc =
     <B>(encB: Encoder<B>) =>
     <C>(encC: Encoder<C>): Encoder<ControlFlow<B, C>> =>
@@ -175,6 +320,13 @@ export const enc =
             [breakSymbol]: ([, b]: Break<B>) => encB(b),
             [continueSymbol]: ([, c]: Continue<C>) => encC(c),
         })(([key]) => key)((key) => encU8(key === breakSymbol ? 0 : 1));
+/**
+ * Generates an `Decoder` for a `ControlFlow` object.
+ *
+ * @param decB - An decoder for break value.
+ * @param decC - An decoder for continue value.
+ * @returns A composite decoder.
+ */
 export const dec =
     <B>(decB: Decoder<B>) =>
     <C>(decC: Decoder<C>): Decoder<ControlFlow<B, C>> =>
