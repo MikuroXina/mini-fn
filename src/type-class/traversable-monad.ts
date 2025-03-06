4a8b615f277d15eb24a16bc06ecaf2e63162a6e4
MikuroXina <10331164+MikuroXina@users.noreply.github.com>
2025-03-06 09:51:01 +0900

    Add doc for TraversableMonad

--- src/type-class/traversable-monad.ts
+++ src/type-class/traversable-monad.ts
@@ -1,4 +1,7 @@
 import type { Monad } from "./monad.js";
 import type { Traversable } from "./traversable.js";
 
+/**
+ * A structure which can be traversed with {@link TypeClass.Traversable.Traversable | `Traversable`} and also composable with {@link TypeClass.Monad.Monad | `Monad`}.
+ */
 export type TraversableMonad<T> = Traversable<T> & Monad<T>;
