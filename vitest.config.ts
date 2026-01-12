52e71d5fdae78bbec5a0e6c721dc98b067d9dbc6
MikuroXina <10331164+MikuroXina@users.noreply.github.com>
2026-01-13 01:32:24 +0900

    Fix doctest include

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,6 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
     plugins: [doctest({})],
     test: {
-        includeSource: ["./src/**/*.[jt]s?(x)"],
+        includeSource: ["./src/**/*.ts"],
     },
 });
