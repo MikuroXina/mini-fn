05da8261305d8e87feedeb6e487c3f6e56aeba4a
MikuroXina <10331164+MikuroXina@users.noreply.github.com>
2026-02-06 23:44:56 +0900

    Enforce PartialOrd

--- src/type-class/partial-ord.ts
+++ src/type-class/partial-ord.ts
@@ -1,6 +1,13 @@
 import type { Get1, Hkt1 } from "../hkt.js";
-import { flatMap, map, mapOr, type Option, some } from "../option.js";
-import { and, equal, isEq, type Ordering } from "../ordering.js";
+import {
+    flatMap,
+    isSomeAnd,
+    map,
+    mapOr,
+    type Option,
+    some,
+} from "../option.js";
+import * as Ordering from "../ordering.js";
 import type { Monoid } from "./monoid.js";
 import type { PartialEq } from "./partial-eq.js";
 import { semiGroupSymbol } from "./semi-group.js";
@@ -12,17 +19,19 @@ import type { Contravariant } from "./variance.js";
  * - Duality: If `f` is `PartialOrd`, for all `a` and `b`; `f(a, b) == -f(b, a)`.
  */
 export type PartialOrd<Lhs, Rhs = Lhs> = PartialEq<Lhs, Rhs> & {
-    readonly partialCmp: (lhs: Lhs, rhs: Rhs) => Option<Ordering>;
+    readonly partialCmp: (lhs: Lhs, rhs: Rhs) => Option<Ordering.Ordering>;
 };
 
 export const fromPartialCmp =
     <Lhs, Rhs, X = void>(
-        partialCmp: (x: X) => (lhs: Lhs, rhs: Rhs) => Option<Ordering>,
+        partialCmp: (x: X) => (lhs: Lhs, rhs: Rhs) => Option<Ordering.Ordering>,
     ) =>
     (x: X): PartialOrd<Lhs, Rhs> => ({
         partialCmp: partialCmp(x),
         eq: (l, r) =>
-            mapOr(false)((order: Ordering) => isEq(order))(partialCmp(x)(l, r)),
+            mapOr(false)((order: Ordering.Ordering) => Ordering.isEq(order))(
+                partialCmp(x)(l, r),
+            ),
     });
 
 export const fromProjection =
@@ -46,14 +55,17 @@ export const contravariant: Contravariant<PartialOrdHkt> = {
 };
 
 export const identity: PartialOrd<unknown> = fromPartialCmp(
-    () => () => some(equal),
+    () => () => some(Ordering.equal),
 )();
 
+/**
+ * The `Monoid` instance for `PartialOrd`'s comparison.
+ */
 export const monoid = <Lhs, Rhs>(): Monoid<PartialOrd<Lhs, Rhs>> => ({
     combine: (x, y) => ({
         partialCmp: (l, r) =>
-            flatMap((first: Ordering) =>
-                map((second: Ordering) => and(second)(first))(
+            flatMap((first: Ordering.Ordering) =>
+                map((second: Ordering.Ordering) => Ordering.and(second)(first))(
                     y.partialCmp(l, r),
                 ),
             )(x.partialCmp(l, r)),
@@ -62,3 +74,82 @@ export const monoid = <Lhs, Rhs>(): Monoid<PartialOrd<Lhs, Rhs>> => ({
     identity,
     [semiGroupSymbol]: true,
 });
+
+/**
+ * Determines the two items are equal, that `ord.partialCmp(lhs, rhs)` is `some(equal)`.
+ *
+ * @param ord - The `PartialOrd` instance for `T`.
+ * @param lhs - Left hand term.
+ * @param rhs - Right hand term.
+ * @returns Whether `ord.partialCmp(lhs, rhs)` is `some(equal)`.
+ */
+export const isEq =
+    <T>(ord: PartialOrd<T>) =>
+    (lhs: T, rhs: T): boolean =>
+        isSomeAnd(Ordering.isEq)(ord.partialCmp(lhs, rhs));
+/**
+ * Determines the two items are not equal, that `ord.partialCmp(lhs, rhs)` is not `some(equal)`.
+ *
+ * @param ord - The `PartialOrd` instance for `T`.
+ * @param lhs - Left hand term.
+ * @param rhs - Right hand term.
+ * @returns Whether `ord.partialCmp(lhs, rhs)` is not `some(equal)`.
+ */
+export const isNe =
+    <T>(ord: PartialOrd<T>) =>
+    (lhs: T, rhs: T): boolean =>
+        !isSomeAnd(Ordering.isEq)(ord.partialCmp(lhs, rhs));
+/**
+ * Determines the left item is less than the right item, that `ord.partialCmp(lhs, rhs)` is `some(less)`.
+ *
+ * @param ord - The `PartialOrd` instance for `T`.
+ * @param lhs - Left hand term.
+ * @param rhs - Right hand term.
+ * @returns Whether `ord.partialCmp(lhs, rhs)` is `some(less)`.
+ */
+export const isLt =
+    <T>(ord: PartialOrd<T>) =>
+    (lhs: T, rhs: T): boolean =>
+        isSomeAnd(Ordering.isLt)(ord.partialCmp(lhs, rhs));
+/**
+ * Determines the left item is greater than the right item, that `ord.partialCmp(lhs, rhs)` is `some(greater)`.
+ *
+ * @param ord - The `PartialOrd` instance for `T`.
+ * @param lhs - Left hand term.
+ * @param rhs - Right hand term.
+ * @returns Whether `ord.partialCmp(lhs, rhs)` is `some(greater)`.
+ */
+export const isGt =
+    <T>(ord: PartialOrd<T>) =>
+    (lhs: T, rhs: T): boolean =>
+        isSomeAnd(Ordering.isGt)(ord.partialCmp(lhs, rhs));
+/**
+ * Determines the left item is less than or equal to the right item, that `ord.partialCmp(lhs, rhs)` is `some(less)` or `some(equal)`.
+ *
+ * @param ord - The `PartialOrd` instance for `T`.
+ * @param lhs - Left hand term.
+ * @param rhs - Right hand term.
+ * @returns Whether `ord.partialCmp(lhs, rhs)` is `some(less)` or `some(equal).
+ */
+export const isLe =
+    <T>(ord: PartialOrd<T>) =>
+    (lhs: T, rhs: T): boolean =>
+        isSomeAnd(
+            (order: Ordering.Ordering) =>
+                Ordering.isLt(order) || Ordering.isEq(order),
+        )(ord.partialCmp(lhs, rhs));
+/**
+ * Determines the left item is greater than or equal to the right item, that `ord.partialCmp(lhs, rhs)` is `some(greater)` or `some(equal)`.
+ *
+ * @param ord - The `PartialOrd` instance for `T`.
+ * @param lhs - Left hand term.
+ * @param rhs - Right hand term.
+ * @returns Whether `ord.partialCmp(lhs, rhs)` is `some(greater)` or `some(equal).
+ */
+export const isGe =
+    <T>(ord: PartialOrd<T>) =>
+    (lhs: T, rhs: T): boolean =>
+        isSomeAnd(
+            (order: Ordering.Ordering) =>
+                Ordering.isGt(order) || Ordering.isEq(order),
+        )(ord.partialCmp(lhs, rhs));
