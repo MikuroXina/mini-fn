b7632e32a8022fdea2e9d8caf0a14b7a6aeb7c51
MikuroXina <10331164+MikuroXina@users.noreply.github.com>
2026-07-16 01:12:44 +0900

    Format

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["mod.ts"],
-  format: "esm",
-  dts: {
-    compilerOptions: {
-      ignoreDeprecations: "6.0",
+    entry: ["mod.ts"],
+    format: "esm",
+    dts: {
+        compilerOptions: {
+            ignoreDeprecations: "6.0",
+        },
     },
-  },
 });
